b925f10e299d9a7cc30e45d25ee03e12d7e53662
DoubleThought <DoubleThoughtTheProgrammer@gmail.com>
2023-08-05 14:58:40 +0800

    Move character system into its own folder

--- SceneStateMachine.gd
+++ SceneStateMachine.gd
@@ -2,7 +2,7 @@ extends Node
 
 enum scenes {SCENE_NONE, SCENE_MENU, SCENE_EDITOR, SCENE_SIMULATION}
 
-var scene_editor = preload("res://Editor.tscn")
+var scene_editor = preload("res://EditorSystem/Editor.tscn")
 var scene_simulation = preload("res://Simulation.tscn")
 
 var active_scene = scenes.SCENE_NONE
