7c733efb1024b9001e5ca3e8c571484900f912ba
0111narwhalz <0111narwhalz@gmail.com>
2023-08-26 11:28:46 -0700

    feat: mitigate issue 1

--- ConstructTranslator.gd
+++ ConstructTranslator.gd
@@ -17,13 +17,14 @@ static func to_world(proto_construct):
 #		block.to_world()
 		# Add the block to CoM and moment of inertia
 		block_mass = block.get_meta("mass")
-		com = (com * mass + block.position * block_mass) / (mass + block_mass)
-		block_moment = Vector3(
-			block_mass * (block.position.y*block.position.y + block.position.z*block.position.z),
-			block_mass * (block.position.x*block.position.x + block.position.z*block.position.z),
-			block_mass * (block.position.x*block.position.x + block.position.y*block.position.y)
-			)
-		moment += block_moment
+#		com = (com * mass + block.position * block_mass) / (mass + block_mass)
+#
+#		block_moment = Vector3(
+#			block_mass * (block.position.y*block.position.y + block.position.z*block.position.z),
+#			block_mass * (block.position.x*block.position.x + block.position.z*block.position.z),
+#			block_mass * (block.position.x*block.position.x + block.position.y*block.position.y)
+#			)
+#		moment += block_moment
 		mass += block_mass
 		# Move the block's CollisionShape3D to the RigidBody preserving transform
 		block.get_node("Area3D/CollisionShape3D").reparent(construct, true)
@@ -35,12 +36,13 @@ static func to_world(proto_construct):
 		mass * (com.x*com.x + com.z*com.z),
 		mass * (com.x*com.x + com.y*com.y)
 		)
-	construct.set_center_of_mass_mode(RigidBody3D.CENTER_OF_MASS_MODE_CUSTOM)
-	construct.set_center_of_mass(com)
+#	construct.set_center_of_mass_mode(RigidBody3D.CENTER_OF_MASS_MODE_CUSTOM)
+#	construct.set_inertia(moment)
 	construct.set_mass(mass)
-	construct.set_inertia(moment)
+#	construct.set_center_of_mass(com)
 	proto_construct.queue_free()
 	
+	print(construct.get_center_of_mass())
 	return construct
 
 static func to_editor(construct):
