07cdfbd65b675f60155eeb2aa5d9caae2115cfad
Double <DoubleThoughtTheProgrammer@gmail.com>
2023-11-02 19:09:51 +0800

    Add mathematical framework for inductance

--- BlockSystem/BlockModules/Electricity/ElectricalSimpleElement.gd
+++ BlockSystem/BlockModules/Electricity/ElectricalSimpleElement.gd
@@ -6,24 +6,35 @@ enum ConnectionType { SOURCE, SINK, OBSERVE }
 signal electrical_element_connected(element : ElectricalSimpleElement)
 signal electrical_element_disconnected(element : ElectricalSimpleElement)
 
+### @type {resistance}
 @export_range(1e-12, 100, 1, "or_greater", "suffix:ohms") var resistance : float = 1e-12
+### @type {inductance}
 @export_range(0, 100, 1, "or_greater", "suffix:henry") var inductance : float = 1e-9
 
+# Reduces in charge when current is positive
 ### @type {WeakRef<ElectricalNode>}
 var source_end : WeakRef = weakref(null)
+
+# Increases in charge when current is positive
 ### @type {WeakRef<ElectricalNode>}
 var sink_end : WeakRef = weakref(null)
 
-var current_from_voltage : Callable = resistor_current_from_voltage.bind(resistance);
+# Maps from voltage drop to effective resistance
+### @type {(voltage) -> resistance}
+var get_effective_resistance : Callable = make_constant_resistor.bind(resistance);
 
 var has_been_processed_this_tick : bool = false
 
+### @type {current}
 @export_range(0, 0, 0, "or_less", "or_greater", "hide_slider", "suffix:amperes") \
 	var current : float = 0
 
+# First differential of current in respect to time
+### @type {current / seconds}
 var delta_current : float = 0
 
-func _on_electrical_update(delta_time : float):
+### @param {seconds} delta_time
+func _on_electrical_update(delta_time : float) -> void:
 	var source_voltage : float = 0
 	var sink_voltage : float = 0
 	var source_node : ElectricalNode = source_end.get_ref() as ElectricalNode
@@ -35,14 +46,21 @@ func _on_electrical_update(delta_time : float):
 		sink_voltage = sink_node.get_voltage()
 	
 	var voltage_drop : float = source_voltage - sink_voltage
-	var current : float = current_from_voltage.call(voltage_drop)
-	var delta_charge : float = current * delta_time
+	var effective_resistance = get_effective_resistance.call(voltage_drop)
+	if not (effective_resistance is float):
+		assert(false, "get_effective_resistance was set to a function of a signature other than (float) -> float")
+	
+	var last_current : float = current
+	current = voltage_drop / effective_resistance
+	var correction_from_induction : float = get_inductance_correction(delta_time)
+	var delta_charge : float = current * delta_time + correction_from_induction
 	if source_node != null:
 		source_node._change_charge_by(-delta_charge)
 	if sink_node != null:
 		sink_node._change_charge_by(delta_charge)
+	delta_current = delta_current * exp(-delta_time * resistance / inductance) + (current - last_current) / delta_time
 
-func _on_after_electrical_update():
+func _on_pre_electrical_update() -> void:
 	has_been_processed_this_tick = false
 
 func connect_electrical_nodes(source : ElectricalNode, sink : ElectricalNode) -> void:
@@ -62,6 +80,12 @@ func connect_electrical_nodes(source : ElectricalNode, sink : ElectricalNode) ->
 	
 	electrical_element_connected.emit(self)
 	electrical_element_connected.emit(self)
+
+func get_inductance_correction(delta_time : float):
+	var R_over_L : float = resistance / inductance
+	var L_over_R : float = inductance / resistance
+	var Lsq_over_Rsq : float = L_over_R * L_over_R
+	return (exp(-delta_time * R_over_L) - 1) * Lsq_over_Rsq * delta_current
 
 func disconnect_electrical_nodes():
 	var source_element = source_end.get_ref() as ElectricalNode
@@ -83,7 +107,8 @@ func disconnect_electrical_nodes():
 	if source_element == null and sink_element == null:
 		push_warning("[ElectricalSystem]", "Attempting to disconnect already-disconnected element.", get_stack())
 
-static func resistor_current_from_voltage(resistance : float, voltage : float):
-	# V = IR
-	# I = V / R
-	return voltage / resistance;
+static func make_constant_resistor(resistance : float, _voltage : float):
+	return resistance
+
+static func make_piecewise_linear_diode(forward_resistance : float, backwards_resistance : float, voltage: float):
+	return forward_resistance if voltage > 0 else backwards_resistance
