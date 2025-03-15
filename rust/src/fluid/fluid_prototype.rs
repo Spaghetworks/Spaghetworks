36c965e16c01ba872fbd0e8b259633f5924f197f
DoubleThought <74284809+DoubleThoughtTheProgrammer@users.noreply.github.com>
2025-03-15 16:35:20 +0800

    Fluid, a structure that holds a volume of a single fluid

--- rust/src/fluid/fluid_prototype.rs
+++ rust/src/fluid/fluid_prototype.rs
@@ -1,12 +1,37 @@
 use godot::prelude::*;
 
-use crate::physics_units::Density;
+use crate::physics_units::{Density, Mass, Volume};
 
 #[derive(GodotClass)]
 #[class(base=Resource)]
 #[non_exhaustive]
-struct FluidPrototype {
+pub struct FluidPrototype {
     base: Base<Resource>,
 
-    density: Density,
+    pub(crate) density: Density,
+}
+
+#[derive(Clone)]
+pub(crate) struct Fluid {
+    pub(crate) prototype: Gd<FluidPrototype>,
+    pub(crate) volume: Volume,
+}
+
+impl Fluid {
+    pub(crate) fn get_mass(&self) -> Mass {
+        self.prototype.bind().density * self.volume
+    }
+
+    // Err(()) if the volume is more than the current volume
+    pub(crate) fn split_off(&mut self, volume: Volume) -> Result<Fluid, ()> {
+        if self.volume < volume {
+            self.volume -= volume;
+            Ok(Fluid {
+                prototype: self.prototype.clone(),
+                volume: volume,
+            })
+        } else {
+            Err(())
+        }
+    }
 }
